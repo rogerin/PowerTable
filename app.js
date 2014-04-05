c92e2a05ca087d286640316f2b14d692b270b0a2
Rogerio Alencar Filho <geriofilho@gmail.com>
2014-04-04 21:21:24 -0300

    Internacionalização em andamento

--- app.js
+++ app.js
@@ -6,11 +6,21 @@
 var express = require('express');
 var load = require('express-load');
 var http = require('http');
+var i18n = require('i18next');
+
+i18n.init({
+	//saveMissing: true,
+	debug: true,
+	fallbackLng: "pt_BR",
+	ignoreRoutes: ['public/']
+});
+
 
 var app = express();
 
 
 // all environments
+app.use(i18n.handle);
 app.set('views', __dirname + '/views'); //alterado
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -20,6 +30,8 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(__dirname+'/public')); //alterado
+
+i18n.registerAppHelper(app);
 
 // development only
 if ('development' == app.get('env')) {
